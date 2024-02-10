57c86afded44ed61d89a7d535e78f8444eda6b61
Vedarth1 <vedarthambilkar2@gmail.com>
2024-02-10 11:58:53 +0530

    xyz2

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express=require("express");
 const router=express.Router();
 
 const {auth,isStudent,isAdmin}=require("../middleware/auth");
-const {login,signup} = require("../Controllers/Auth.js");
+const {login,signup} = require("../Controllers/auth.js");
 const {forgotpassword}=require("../Controllers/forgotpassword");
 const {resetpassword}=require("../Controllers/resetPassword");
 const {fileUpload}=require("../Controllers/fileUpload");
